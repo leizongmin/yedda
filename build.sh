aed11c961b7b71004bd8a0170ab542ad63c9d352
Zongmin Lei <leizongmin@gmail.com>
2018-08-09 16:13:50 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-vgo build -o release/slimiter-server ./cmd/slimiter-server/
+PACKAGE=./cmd/slimiter-server/
+RELEASE_PATH=release/slimiter-server
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 vgo build -o $RELEASE_PATH-darwin $PACKAGE
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 vgo build -o $RELEASE_PATH-linux $PACKAGE
+CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 vgo build -o $RELEASE_PATH-freebsd $PACKAGE
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 vgo build -o $RELEASE_PATH-windows.exe $PACKAGE
