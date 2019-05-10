e011980d01216f80a610aed361872da06b1618e1
Zongmin Lei <leizongmin@gmail.com>
2019-05-10 13:58:13 +0800

    feat: 添加命令行程序

---
+++ cmd/yedda/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/leizongmin/yedda/server"
+)
+
+func main() {
+
+	options := server.Options{}
+	flag.StringVar(&options.Network, "listen-type", server.DefaultListenNetwork, "listen type, 'tcp' OR 'unix'")
+	flag.StringVar(&options.Address, "listen", server.DefaultListenAddress, "listen address")
+	flag.BoolVar(&options.EnableLog, "log", true, "enable log output")
+	var dbSize, timeAccuracy uint64
+	flag.Uint64Var(&dbSize, "size", 256, "how many database")
+	flag.Uint64Var(&timeAccuracy, "accuracy", 100, "time accuracy (ms)")
+	flag.Parse()
+
+	options.DatabaseSize = uint32(dbSize)
+	options.TimeAccuracy = time.Duration(timeAccuracy) * time.Millisecond
+
+	s, err := server.NewServer(options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if options.EnableLog {
+		go func() {
+			lastC := s.GetConnectionCount()
+			for {
+				c := s.GetConnectionCount()
+				if c != lastC {
+					s.Log("[main]", "There are currently %d connections", c)
+				}
+				lastC = c
+				time.Sleep(time.Second)
+			}
+		}()
+	}
+	s.Loop()
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/leizongmin/yedda
 
 require (
-	github.com/davecgh/go-spew v1.1.0
-	github.com/pmezard/go-difflib v1.0.0
-	github.com/stretchr/objx v0.1.1
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.2.2
 )
