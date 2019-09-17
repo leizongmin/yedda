aa430a1e32a067a6f26811656baca13af0cb54ce
Zongmin Lei <leizongmin@gmail.com>
2019-09-17 20:53:04 +0800

    fix(build.sh): remove vgo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 PACKAGE=./cmd/yedda/
 RELEASE_PATH=release/yedda
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 vgo build -o $RELEASE_PATH-darwin $PACKAGE
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 vgo build -o $RELEASE_PATH-linux $PACKAGE
-CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 vgo build -o $RELEASE_PATH-freebsd $PACKAGE
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 vgo build -o $RELEASE_PATH-windows.exe $PACKAGE
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH-darwin $PACKAGE
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH-linux $PACKAGE
+CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH-freebsd $PACKAGE
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $RELEASE_PATH-windows.exe $PACKAGE
